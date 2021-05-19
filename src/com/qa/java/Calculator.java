f6f6a6bc5bacacf64ec48f40fff493a225e844e6
FARHA <FARHANA.HAI@YAHOO.COM>
2021-05-18 20:29:54 -0500

    msg

---
+++ src/com/qa/java/Calculator.java
@@ -0,0 +1,29 @@
+package com.qa.java;
+
+public class Calculator {
+
+	
+	
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int mult(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	//no floating point values!!
+	public static int div(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+	public static int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public static int mod(int num1, int num2) {
+		return num1 % num2;
+	}
+
+
+}
---
+++ src/com/qa/java/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.qa.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+    	System.out.println("Hello World");
+		System.out.print("Hello World");
+		System.out.print(" Hello World");
+		
+		
+		String myString = "This is my string";
+	    System.out.println(myString);
+		 
+//		 int myInt;
+	//	 myInt = 7;
+//		 float myfloat = 3.7f;
+//		 
+//		 System.out.println(myInt);
+//			System.out.println(myfloat);
+
+	}
+
+}
